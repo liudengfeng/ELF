85643c03f34911216d7ddc92bd515097fb5faf17
liudengfeng <liudengfeng_sd@outlook.com>
2021-05-05 13:19:02 +0800

    +

--- codetools/check_py_style.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# cd to parent of script's directory (i.e. project root)
-cd "${0%/*}"/..
-
-flake8 .
--- scripts/devmode_set_pythonpath.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-export ELF_DEVELOPMENT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
-echo $ELF_DEVELOPMENT_ROOT
-export PYTHONPATH="${ELF_DEVELOPMENT_ROOT}"/src_py/:"${ELF_DEVELOPMENT_ROOT}"/build/elf/:"${ELF_DEVELOPMENT_ROOT}"/build/elfgames/go/:${PYTHONPATH}
-echo $PYTHONPATH
